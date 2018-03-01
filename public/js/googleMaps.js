d3b888b1f219365b9b5cfcd02e32df35267e69e3
Allen K Chiang <akchiang@ucsd.edu>
2018-03-01 02:52:57 -0800

    API lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+  const ucsd_coords = {lat:32.878809, lng:-117.235901};
+
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_coords,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: ucsd_coords,
+    map: map,
+    title:"Student Services Center"
+  });
 }
